d0d65254005cbcaddd723d669231e4d895e1491c
mouad <mouadboumahdicode@gmail.com>
2025-05-14 15:23:08 +0100

    first push in second branch

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:couchup/model/movie.dart';
 import 'package:couchup/services/now_playing_service.dart';
-import 'package:couchup/services/top_rated_service.dart';
-import 'package:couchup/services/popular_service.dart';
-import 'package:couchup/services/upcoming_service.dart';
+import 'package:flutter/material.dart';
+// import 'package:couchup/services/top_rated_service.dart';
+// import 'package:couchup/services/popular_service.dart';
+// import 'package:couchup/services/upcoming_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,28 +13,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final NowPlayingService _nowPlayingService = NowPlayingService();
-  final TopRatedService _topRatedService = TopRatedService();
-  final PopularService _popularService = PopularService();
-  final UpcomingService _upcomingService = UpcomingService();
-  List<dynamic> _movies = [];
+  late Future<List<Movie>> nowPlaying;
+
 
   @override
   void initState() {
     super.initState();
-    _loadMovies();
+    nowPlaying = NowPlayingService().getNowPlayingMovies();
   }
 
-  Future<void> _loadMovies() async {
-    final now_playing_movies = await _nowPlayingService.getNowPlayingMovies();
-    final top_rated_movies = await _topRatedService.getTopRatedMovies();
-    final popular_movies = await _popularService.getPopularMovies();
-    final upcoming_movies = await _upcomingService.getUpcomingMovies();
-    setState(() {
-      _movies = [...now_playing_movies, ...top_rated_movies, ...popular_movies, ...upcoming_movies];
-      // print(_movies);
-    });
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -41,22 +30,34 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("home page"),
         centerTitle: true,
+        leading: Icon(Icons.menu),
+        actions:const [
+          Icon(Icons.search),
+          SizedBox(width: 20,),
+          Icon(Icons.bookmark),
+          SizedBox(width: 10,),
+        ],
       ),
-      body: _movies.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: _movies.length,
-              itemBuilder: (context, index) {
-                final movie = _movies[index];
-                return ListTile(
-                  title: Text(movie['title']),
-                  leading: Image.network(
-                    'https://image.tmdb.org/t/p/w500${movie['poster_path']}',
-                  ),
-                  subtitle: Text(movie['overview']),
-                );
-              },
-            ),
+      body: Padding(padding: EdgeInsets.all(8),
+      child: Column(
+        children: [
+          Text("Now Playing",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Colors.black
+          ),),
+          SizedBox(height: 10,),
+          FutureBuilder(future: nowPlaying, builder: (context,snapshot){
+            if(snapshot.hasData){
+               return Center(child: CircularProgressIndicator(),);
+            }
+          })
+        ],
+      ),
+      ),
+      
+
     );
   }
 }
--- lib/services/now_playing_service.dart
+++ lib/services/now_playing_service.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
+import 'package:couchup/model/movie.dart';
 import 'package:http/http.dart' as http;
 
 class NowPlayingService {
   final String _baseUrl = 'https://api.themoviedb.org/3/movie/now_playing';
   final String _apiKey = '09bc3f9416e1dc316dbfff080ffe843d';
 
-  Future<List<dynamic>> getNowPlayingMovies() async {
+  Future<List<Movie>> getNowPlayingMovies() async {
     final response = await http.get(Uri.parse('$_baseUrl?api_key=$_apiKey'));
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
